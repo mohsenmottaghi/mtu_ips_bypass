5fecb7075b7ce2059619f83f9d00e76d1f03b89d
Mohsen Mottaghi <mohsenmottaghi@outlook.com>
2019-07-06 22:13:16 +0430

    Refactor script for version 1.1 by Arash Shams PMs

--- m-bipf.sh
+++ m-bipf.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 # Bypass IPSs with Packet Fragmentation | Developed by MOHSEN
-
+BANNER='BIPF	|	Version 1.1	|	MOHSEN MOTTAGHI		twitter.com/motmohsen'
 PATH='/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin'
+DEVICE=`ip route list | grep default | awk '{print $5}'`
 
-BANNER='BIPF	|	Version 1.0	|	MOHSEN MOTTAGHI		twitter.com/motmohsen'
-DISTRO=`lsb_release -i |cut -f 2`
-DISTRO_RELEASE=`lsb_release -r |cut -f 2`
-FILE_DNS_SERVER='/tmp/cloudflared-stable-linux-amd64.deb'
-
-echo 'Welcome to M-BIPF'
-
-function debian_base {
-	if [ ! -f "$FILE_DNS_SERVER" ]
+debian_base() {
+	if [ ! -f "/tmp/cloudflared-stable-linux-amd64.deb" ]
 		then			
-			echo '[+] Start downloading files'
-			echo '[+] Please wait ...'
+			echo '[+] Start downloading files. Please wait ...'
 			wget -P /tmp -o /dev/null https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
 			echo '[+] Done.'
 		else
@@ -23,107 +16,73 @@ function debian_base {
 
 	if ! [ -x "$(command -v cloudflared)" ]
 		then
-			echo '[+] Start Installing'
+			echo '[+] Start Installing .deb package.'
 			sudo dpkg -i /tmp/cloudflared-stable-linux-amd64.deb
 			echo '[+] Done.'
 	fi
 }
 
-function start_dns_server {
+start() {
 	sudo cloudflared proxy-dns &
-	cp /etc/resolv.conf /tmp/resolv.conf
-	sudo echo 'nameserver 127.0.0.1' > /etc/resolv.conf
-}
-
-function setup_network {
-	DEVICE=`ip route list | grep default | awk '{print $5}'`
+	sudo cp /etc/resolv.conf /tmp/resolv.conf && sudo echo 'nameserver 127.0.0.1' > /etc/resolv.conf
 	sudo ip link set dev $DEVICE mtu 400
 }
 
-function recovery {
-	echo '[+] Start to Recovery'
-	DEVICE=`ip route list | grep default | awk '{print $5}'`
+stop() {
+	echo '[+] Try to stop service and set default values'
 	sudo ip link set dev $DEVICE mtu 1400
 	sudo cp /tmp/resolv.conf /etc/resolv.conf
+	sudo kill -15 `ps aux |grep "cloudflared proxy-dns" | grep -v grep | awk '{print $2}'`
 	echo '[+] Done.'	
 }
 
-function uninstall {
-	echo '[+] Start unistall packages.'
+uninstall() {
+	stop && echo 'unistall packages.'
 	sudo dpkg --remove cloudflared
-	echo '[+] Done'
-	echo '[+] Stop Processes'
-	sudo kill -15 `ps aux |grep "cloudflared proxy-dns" | grep -v grep | awk '{print $2}'`
-	echo '[+] Process Killed'
-	echo '[+] strat Recovery'
-	recovery
+	echo '[+] Done.'
 }
 
-function help {
-	echo $BANNER
-	echo 'Help:'
-	echo '		--help , -h , help		For show help of shell script'
-	echo '		 '
-	echo '		install   , --install		For installing tools and configure network'
-	echo ' 		ghost 	  , --ghost		same function as "install"'
-	echo ' 		uninstall , --uninstall		For uninstall DNS over HTTPS software'
-	echo '		recovery  , --recovery		For recovery network setting to original setting'  
+help() {
+	echo -e 'Welcome to m-bipf\n' $BANNER
+	echo -e '\nHelp:\n'
+	echo ' 		start 	  , --start 		Try to start service'
+	echo '		stop 	  , --stop 		For stop network setting to original setting'  
+	echo '		install   , --install 		For installing tools and configure network'
+	echo ' 		uninstall , --uninstall 	For uninstall DNS over HTTPS software'
 }
 
-function main_func {
-	case $DISTRO in
+main_func() {
+	case `lsb_release -i |cut -f 2` in
 	
-		'Ubuntu')
-			if [ "$DISTRO_RELEASE" == "18.04" ]
+		'Ubuntu' | 'Kali')
+			if [ "`lsb_release -r |cut -f 2`" == "18.04" ]
 			then
-				echo '[+] You will be a Ghost after a while :)'
-				debian_base
-				start_dns_server
-				setup_network
+				debian_base && start
 			else
 				echo 'We don`t support now, you can develop and ...'
 			fi
 			;;
-
-                'Kali')
-                        if [ "$DISTRO_RELEASE" == "2019.2" ]
-                        then
-                                echo '[+] You will be a Ghost after a while :)'
-                                debian_base
-                                start_dns_server
-                                setup_network
-                        else
-                                echo 'We don`t support now, you can develop and ...'
-                        fi
-                        ;;
-
 		*)
 			echo 'We don`t support now, you can develop and ...'
-			exit
 			;;
 	esac
 }
 
 case $1 in
 
-	'install' | 'ghost' | '--install' | '--ghost' )
-		main_func
-		exit
+	'install' | '--install' )
+		main_func && start && exit
 		;;
-
 	'uninstall' | '--uninstall')
-		uninstall
-		exit
+		uninstall && exit
 		;;
-
-	'recovery' | '--recovery')
-		recovery
-		exit		
+	'stop' | '--stop')
+		stop && exit		
 		;;
-
-	* | '--help' | '-h' | 'help')
-		help
-		exit
-		;; 
-
+	'start' | '--start')
+		start && exit		
+		;;
+	*)
+		help && exit
+		;;
 esac
